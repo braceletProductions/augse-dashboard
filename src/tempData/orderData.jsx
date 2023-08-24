13b2cd620a520fbb9490e73f2d7da8fbfe01d4ab
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-24 22:59:19 +0530

    done

--- src/tempData/orderData.jsx
+++ src/tempData/orderData.jsx
@@ -1,10 +1,10 @@
 const orderData = {
   totalOrders: 1230,
   canceled: 23,
-  returned: 12,
+  returned: 990,
   delivered: 1320,
   shipped: 1320,
-  pending: 1320,
+  pending: 1000,
 };
 
 export default orderData;
