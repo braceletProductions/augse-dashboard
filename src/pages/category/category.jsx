bd3f7fba7fb35533be2c4c42656f3ec0851a0974
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-25 00:59:33 +0530

    done

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -4,28 +4,28 @@ import Link from "next/link";
 
 const Category = () => {
   return (
-    <div className="flex flex-wrap  gap-6 p-4 justify-center text-gray-100">
+    <div className="flex flex-wrap gap-6 p-4 justify-center items-center text-gray-100">
       {varietyData.map((category) => (
         <div
           key={category.id}
-          className="w-30  h-21 p-4  bg-gray-100  flex flex-col justify-between"
+          className="bg-gray-100 flex flex-col justify-between"
         >
           <p>
             <img
               src={category.imageUrl}
               alt={category.name}
-              className="max-w-full h-auto "
+              className="w-full h-1/2 object-cover"
             />
             <Link href={`/category/${category.id}`} passHref>
-              <h3
-                className="text-lg font-semibold my-2 text-center "
-                style={{ color: "rgb(27, 72, 121)" }}
-              >
-                {category.name}
-              </h3>
-              <p className="text-sm text-gray-500 text-center">
-                {category.description}
-              </p>
+              <div className="h-1/2 text-center">
+                <h3
+                  className="text-lg font-semibold my-2"
+                  style={{ color: "rgb(27, 72, 121)" }}
+                >
+                  {category.name}
+                </h3>
+                <p className="text-sm text-gray-500">{category.description}</p>
+              </div>
             </Link>
           </p>
         </div>
