f6878107d3e8777e846d0c4d12a2d4d52494e953
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-24 21:39:28 +0530

    fixed layout

--- src/pages/return/return.jsx
+++ src/pages/return/return.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Link from "next/link";
+
 import customerData from "@/tempData/customerData";
-const returnOrders = () => {
+import categoryData from "@/tempData/categoryData";
+import productData from "@/tempData/productData";
+
+const Shipping = () => {
   return (
     <div className="flex-1">
       <div className="mt-10 mb-12 px-4">
@@ -18,23 +23,30 @@ const returnOrders = () => {
                   <h1 className="text-lg font-semibold text-blue-900">
                     {customer.name}
                   </h1>
-                  <p className="text-sm text-gray-600 text-blue-400">
-                    {customer.email}
-                  </p>
+                  <p className="text-sm  text-blue-500">{customer.email}</p>
                 </div>
                 <div className="pl-5 pr-7 py-2 flex-col">
-                  <h2 className="text-base font-semibold text-gray-900">
-                    Product:
+                  <h2 className="text-base font-semibold text-xl text-blue-900">
+                    {categoryData.find(
+                      (category) => category.categoryId === customer.productId
+                    )?.category || "Unknown Category"}
                   </h2>
+                  <p className="text-blue-500">
+                    {productData.find(
+                      (product) => product.id === customer.productId
+                    )?.description || "No description available"}
+                  </p>
                 </div>
-                <div className="pr-7">
-                  <a
-                    href={`c/${customer.productId}`}
-                    className="bg-blue-900 text-white px-14 py-2 rounded-3xl lg:mt-0 sm:mt-3"
+                <div className="pr-7 flex flex-col mt-3 sm:flex-row sm:space-x-3">
+                  <Link
+                    href={`/product/[productId]`}
+                    as={`/product/${customer.productId}`}
                   >
-                    Product Detail
-                  </a>
-                  <button className="bg-blue-900 text-white px-14 py-2 rounded-3xl lg:mt-0 sm:mt-3">
+                    <div className="bg-blue-900 text-white px-10 py-2 rounded-3xl">
+                      Product Detail
+                    </div>
+                  </Link>
+                  <button className="bg-blue-900 text-white px-10 py-2 rounded-3xl mt-3 sm:mt-0">
                     Track
                   </button>
                 </div>
@@ -47,4 +59,4 @@ const returnOrders = () => {
   );
 };
 
-export default returnOrders;
+export default Shipping;
--- src/pages/shipping/shipping.jsx
+++ src/pages/shipping/shipping.jsx
@@ -23,9 +23,7 @@ const Shipping = () => {
                   <h1 className="text-lg font-semibold text-blue-900">
                     {customer.name}
                   </h1>
-                  <p className="text-sm text-gray-600 text-blue-400">
-                    {customer.email}
-                  </p>
+                  <p className="text-sm  text-blue-500">{customer.email}</p>
                 </div>
                 <div className="pl-5 pr-7 py-2 flex-col">
                   <h2 className="text-base font-semibold text-xl text-blue-900">
@@ -33,6 +31,11 @@ const Shipping = () => {
                       (category) => category.categoryId === customer.productId
                     )?.category || "Unknown Category"}
                   </h2>
+                  <p className="text-blue-500">
+                    {productData.find(
+                      (product) => product.id === customer.productId
+                    )?.description || "No description available"}
+                  </p>
                 </div>
                 <div className="pr-7 flex flex-col mt-3 sm:flex-row sm:space-x-3">
                   <Link
