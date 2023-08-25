05e68c60bf065e2f990fd3eec2e56fbb43c36fa0
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-08-25 18:36:20 +0530

    Dashboard

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -11,17 +11,21 @@ const menuItems = [
   { label: "Track Orders", icon: <FaClipboardList className="text-2xl  " /> },
 ];
 
-const MenuItems = () => {
+const MenuItems = (props) => {
   return (
     <div className="flex-grow flex flex-col pl-4 pr-4 lg:w-full">
       <div className="flex flex-col justify-center text-center bg-gray-100 rounded-3xl   pt-1 pb-1 mt-1 mb-1 lg:flex-row lg:justify-center lg:flex-wrap">
         {menuItems.map((item, index) => (
           <button
             key={index}
-            className="flex  gap-1  bg-blue-500 text-white pr-1 pl-2 pr-7 mr-28 pt-2 pb-2 ml-4 text-xl mb-1 lg:mb-0 lg:mr-10 lg:ml-1 lg:w-42 lg:pl-8 lg:pr-8"
+            className="flex  gap-1  bg-blue-500 text-white pr-1 pl-2 mr-28 pt-2 pb-2 ml-4 text-xl mb-1 lg:mb-0 lg:mr-10 lg:ml-1 lg:w-42 lg:pl-8 lg:pr-8"
           >
             {item.icon} <span className="hidden lg:inline"> </span>
-            {item.label}
+            {item.label} {item.label != "Track Orders" && <span>(</span>}
+            {item.label == "Orders" && props.orders}
+            {item.label == "Users" && props.users}
+            {item.label == "Products" && props.products}
+            {item.label != "Track Orders" && <span>)</span>}
           </button>
         ))}
       </div>
--- src/pages/customers/customer.jsx
+++ src/pages/customers/customer.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useRouter } from "next/router";
 import Sidebar from "@/components/Sidebar";
-import customerData from "@/tempData/customerData";
 
 const Customer = () => {
   const router = useRouter();
-  // const customerData = [
-  //   { id: 1, name: "Liza D'Souza", email: "awserdky.nhtrsdf@gmail.com" },
-  //   { id: 2, name: "Sakshi Sinha", email: "awserdky.nhtrsdf@gmail.com" },
-  //   // Add more customer data entries here
-  // ];
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:4001/api/v1/user/getusers/customer"
+        );
+        setCustomers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
 
   const handleProfileClick = (customerId) => {
     router.push(`/customers/${customerId}`); // Navigate to customer detail page
@@ -17,34 +27,28 @@ const Customer = () => {
 
   return (
     <div className="flex flex-col lg:flex-row min-h-screen mt-5 lg:mt-5">
-      {/* Sidebar */}
-
       <Sidebar />
-
-      {/* Customers Content */}
       <div className="flex-1">
         <div className="mt-10 mb-12 px-4">
           <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-3xl text-gray-100 pb-5">
             Customers
           </h1>
           <div className="flex flex-col">
-            {customerData.map((customer) => (
+            {customers.map((customer) => (
               <div
-                key={customer.id}
+                key={customer._id}
                 className="flex justify-between items-center bg-gray-100 rounded-3xl pt-2 pb-2 mt-1 mb-1"
               >
                 <div className="flex items-start flex-col pl-5">
                   <h1 className="text-lg font-semibold text-blue-900">
                     {customer.name}
                   </h1>
-                  <p className="text-sm text-gray-600 text-blue-400">
-                    {customer.email}
-                  </p>
+                  <p className="text-sm text-blue-400">{customer.email}</p>
                 </div>
                 <div className="pr-7">
                   <button
                     className="bg-blue-900 text-white px-14 py-2 rounded-3xl lg:mt-0 sm:mt-3"
-                    onClick={() => handleProfileClick(customer.id)}
+                    onClick={() => handleProfileClick(customer._id)}
                   >
                     Profile
                   </button>
