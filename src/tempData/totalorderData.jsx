7af876316785003d31983f75ec68733360f575e0
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-08-26 10:26:27 +0530

    deliveryorder totalorder

---
+++ src/tempData/totalorderData.jsx
@@ -0,0 +1,34 @@
+// customerData.js
+const customerData = [
+    {
+        id: 1,
+        name: "Liza D'Souza",
+        email: "awserdky.nhtrsdf@gmail.com",
+        sareename: "Pure Silk Saree",
+        desc: "Description"
+    },
+    {
+        id: 2,
+        name: "Liza D'Souza",
+        email: "awserdky.nhtrsdf@gmail.com",
+        sareename: "Pure Silk Saree",
+        desc: "Description"
+    },
+    {
+        id: 3,
+        name: "Liza D'Souza",
+        email: "awserdky.nhtrsdf@gmail.com",
+        sareename: "Pure Silk Saree",
+        desc: "Description"
+    },
+    {
+        id: 4,
+        name: "Liza D'Souza",
+        email: "awserdky.nhtrsdf@gmail.com",
+        sareename: "Pure Silk Saree",
+        desc: "Description"
+    }
+    // Add more customer data entries here
+];
+
+export default customerData;
