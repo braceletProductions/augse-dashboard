856a5c00f37f50ced722dc93f03f774e3f414ea1
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-08-27 17:07:13 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Success from "@/components/success";
 import Unsuccess from "@/components/unsuccess";
+import Login from "./login";
 const Home = () => {
   return (
     <div>
       <dashboard />
 
       {/* <Success /> */}
-      <Unsuccess />
+      {/* <Unsuccess /> */}
+
+      <Login />
     </div>
   );
 };
