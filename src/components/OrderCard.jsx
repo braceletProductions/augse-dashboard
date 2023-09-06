5c7a2c3731802101145ddac11812f472078608d3
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-09-06 16:37:07 +0530

    Order card

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -10,24 +10,26 @@ function OrderCard(props) {
 
   return (
     <div className="mx-auto min-h-[5rem] my-[1rem] rounded-lg flex justify-between items-center p-[1rem] bg-white">
-      <div className="">
-        <h1 className="text-4xl font-semibold text-blue-900 mb-2">
+      <div className="flex-[1]">
+        <h1 className="md:text-4xl text-2xl font-semibold text-blue-900 mb-2">
           {props.name}
         </h1>
-        <p className="text-2xl text-blue-500">{props.email}</p>
+        <p className="md:text-2xl text-lg text-blue-500">{props.email}</p>
       </div>
-      <div className="flex sm:block hidden flex-col text-center">
-        <h1 className="text-4xl font-semibold text-blue-900 mb-2">Payment</h1>
-        <p className="text-2xl text-blue-500">
-          {props.paymentMode} ({props.payment ? "Successful" : "Pending"})
-        </p>
+      <div className="md:flex justify-between items-center flex-[1]">
+        <div className="flex md:block hidden flex-col text-center">
+          <h1 className="text-4xl font-semibold text-blue-900 mb-2">Payment</h1>
+          <p className="text-2xl text-blue-500">
+            {props.paymentMode} ({props.payment ? "Successful" : "Pending"})
+          </p>
+        </div>
+        <button
+          className="bg-blue-900 float-right text-white rounded-lg px-4 h-[3.5rem] sm:text-2xl cursor-pointer shadow-black shadow-md active:shadow-none"
+          onClick={showOrderDetailHandler}
+        >
+          Order Detail
+        </button>
       </div>
-      <button
-        className="bg-blue-900 text-white rounded-lg px-4 py-2 sm:text-2xl cursor-pointer shadow-black shadow-md active:shadow-none"
-        onClick={showOrderDetailHandler}
-      >
-        Order Detail
-      </button>
     </div>
   );
 }
