8b6cd0c3c1a9d05aceaf8df79ef7694c06bda82d
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-22 14:50:37 +0530

    Auth slice

---
+++ store/slices/auth.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userId: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "Auth",
+  initialState: initialState,
+  reducers: {
+    login: (state, action) => {
+      state.userId = action.payload.userId;
+      state.token = action.payload.token;
+    },
+    logout: () => {
+      state.userId = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice;
--- store/store.js
+++ store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import orderReducer from "./slices/orders";
 import productReducer from "./slices/products";
 import uiReducer from "./slices/ui";
+import authSlice from "./slices/auth";
 
 const store = configureStore({
   reducer: {
     orders: orderReducer,
     products: productReducer,
     ui: uiReducer,
+    auth: authSlice,
   },
 });
 
