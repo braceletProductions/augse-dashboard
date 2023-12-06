39e9b113e37972ea32a06d450b7cd8c4ff60e212
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-12-06 14:31:48 +0530

    push

--- src/components/dashboards/Accountant.jsx
+++ src/components/dashboards/Accountant.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import SalesDateRangeForm from "../SalesDateRangeForm";
 import { BsCart4 } from "react-icons/bs";
 import SalesChart from "../SalesChart";
+import { useSelector } from "react-redux";
 
 const Accountant = () => {
-  const OrdersNumber = 120;
+  const orders = useSelector((state) => state.orders.totalOrders) || [];
 
   return (
     <div className="w-full">
       <div className="bg-white w-fit flex justify-center items-center gap-2 px-4 py-2 text-xl ml-20">
-        <BsCart4 /> Orders ({OrdersNumber})
+        <BsCart4 /> Orders ({orders.length})
       </div>
       <div className="mx-8 flex justify-between mt-20 text-black">
-        <SalesChart />
+        {/* <SalesChart /> */}
         <SalesDateRangeForm />
       </div>
     </div>
